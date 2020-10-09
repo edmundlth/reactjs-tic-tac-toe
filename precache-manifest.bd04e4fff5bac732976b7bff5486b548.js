23004d140e172cfdfc5362ea749830a4328263bb
edmundlth <edmundlth@yahoo.com>
2020-10-09 23:25:06 +1100

    Updates

---
+++ precache-manifest.bd04e4fff5bac732976b7bff5486b548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771ef6875396cc49e7105a0ae6321466",
+    "url": "/reactjs-tic-tac-toe/public/index.html/index.html"
+  },
+  {
+    "revision": "15f1b20edf3b9dc477bc",
+    "url": "/reactjs-tic-tac-toe/public/index.html/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "5e6957db64a54f115078",
+    "url": "/reactjs-tic-tac-toe/public/index.html/static/js/2.9f655b67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-tic-tac-toe/public/index.html/static/js/2.9f655b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f1b20edf3b9dc477bc",
+    "url": "/reactjs-tic-tac-toe/public/index.html/static/js/main.097dfc95.chunk.js"
+  },
+  {
+    "revision": "c32e94cabc14858c237b",
+    "url": "/reactjs-tic-tac-toe/public/index.html/static/js/runtime-main.5a58b1c8.js"
+  }
+]);
